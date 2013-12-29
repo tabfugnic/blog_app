3071d20679a5105cf2b56f7e44eb39e37a4c55d6
Eric Collins <tabfugnic@gmail.com>
2013-12-29 10:00:33 -0500

    Bewd Blog

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131229133644) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.text     "content"
     t.string   "email"
